88ba50e6433bbe35c2b1dcab79e57caa3e20d48d
clammier <clammier@gmail.com>
2013-12-17 22:26:53 +0100

    first upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+	$this->User = array(
+		'user'=>'loleventvods',
+		'passwd'=>'vodsteam',
+		'api_type' => 'json'
+	);
+
+	$this->API = array(
+		'gg'=>'http://www.gosugamers.net/api/matches?apiKey=8d444a60d1c27e8113d2c28434f7b004&game=lol',
+		'jd'=>''
+	);
+
+	$this->googleAPI = 'AIzaSyC2Uzk2YTh1xqk2C5NYf2nvbd_0u1c3I5o';
+?>
